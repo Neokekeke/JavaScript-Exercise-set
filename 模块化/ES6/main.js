61eb29664279a1ae48b434ec24cd01c3a974c2dc
Neokekeke <969504161@qq.com>
2018-08-28 18:35:01 +0800

    页面重构项目

---
+++ 模块化/ES6/main.js
@@ -0,0 +1,5 @@
+
+import getName , {name} from './moduleA';
+
+console.log(name);
+getName();
---
+++ 模块化/ES6/moduleA.js
@@ -0,0 +1,6 @@
+
+export var name = 'Neo';
+
+export default function getName () {
+    console.log(name);                              
+}
